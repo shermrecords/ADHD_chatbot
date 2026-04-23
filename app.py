b317b908015492d24395f9d5159b68f266a20267
sherm labs <sherm@sherms-MacBook-Air.local>
2026-04-23 18:55:14 -0400

    fix port binding

--- app.py
+++ app.py
@@ -448,4 +448,5 @@ def chat():
 startup()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.getenv("PORT", 5050))
+    app.run(debug=True, host="0.0.0.0", port=port)
